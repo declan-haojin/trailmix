3dff9d98b699c78a55ba0725fc709ebd99d7624a
Haojin Li <hi@haojin.li>
2024-10-05 00:35:01 -0400

    Fix vercel config

--- client/src/functions/test.js
+++ client/src/functions/test.js
@@ -1,6 +1,6 @@
 export const getTest = async () => {
     try {
-        const response = await fetch("http://localhost:3001/test", {
+        const response = await fetch("/api/test", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
